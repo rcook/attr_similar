1564eb32dd2196834a0051e9e2b004fa032ebca9
Richard Cook <rcook@rcook.org>
2013-12-08 20:31:41 -0800

    Fix Rails 4 Widget model to use strong parameters instead of attr_accessible

--- test/dummy-rails4/app/models/widget.rb
+++ test/dummy-rails4/app/models/widget.rb
@@ -1,15 +1,21 @@
 class Widget < ActiveRecord::Base
-  attr_accessible :address,
-    :birthdate,
-    :first_name,
-    :last_name,
-    :preferred_name
-
   attr_similar [1, 2, 2, 3, 3],
     :address,
     :birthdate,
     :first_name,
     :last_name,
     :preferred_name
+
+  private
+
+  def widget_params
+    params.permit(
+      :address,
+      :birthdate,
+      :first_name,
+      :last_name,
+      :preferred_name
+    )
+  end
 end
 
